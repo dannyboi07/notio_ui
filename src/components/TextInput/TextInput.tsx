2e1877d4844bdddd970f5cad10c14c2ae59be26f
dannyboi07 <danielchettiar07@gmail.com>
2023-06-16 19:40:24 +0530

    Adds box shadow transition to the text input component

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -4,7 +4,7 @@ function TextInput(props: React.ComponentPropsWithoutRef<"input">) {
     return (
         <input
             className={[
-                "rounded-[0.25em] p-2.5 shadow-[0_0_0_1px] shadow-blue-600 focus:shadow-[0_0_0_2px] focus:shadow-blue-800 focus:outline-none",
+                "rounded-[0.25em] p-2.5 shadow-[0_0_0_1px] shadow-blue-600 transition-shadow focus:shadow-[0_0_0_2px] focus:shadow-blue-800 focus:outline-none",
                 props.className ?? "",
             ].join(" ")}
             {...props}
