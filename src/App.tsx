93fb2a06096b2f7dbeb16dfed652b93f494cd237
dannyboi07 <danielchettiar07@gmail.com>
2023-06-16 17:01:29 +0530

    Changes routes and elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import AllBoardsPage from "./pages/AllBoardsPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<div>Home</div>} />
+                <Route path="/" element={<AllBoardsPage />} />
+                <Route path="/login" element={<LoginPage />} />
             </Routes>
         </Router>
     );
