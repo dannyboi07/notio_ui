721ec0c0f0dfcca03536ec88af52ea73e1135396
dannyboi07 <danielchettiar07@gmail.com>
2023-06-16 19:40:10 +0530

    Removes non existing import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
 import AllBoardsPage from "./pages/AllBoardsPage";
 import LoginPage from "./pages/LoginPage";
 
