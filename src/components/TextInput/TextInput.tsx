22784d7d3a7b2e5da188c0f2044384f63c7c49bc
dannyboi07 <danielchettiar07@gmail.com>
2023-06-16 17:03:32 +0530

    Adds `<TextInput />` component

---
+++ src/components/TextInput/TextInput.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function TextInput(props: React.ComponentPropsWithoutRef<"input">) {
+    return (
+        <input
+            className={[
+                "rounded-[0.25em] p-2.5 shadow-[0_0_0_1px] shadow-blue-600 focus:shadow-[0_0_0_2px] focus:shadow-blue-800 focus:outline-none",
+                props.className ?? "",
+            ].join(" ")}
+            {...props}
+        />
+    );
+}
+
+export default TextInput;
