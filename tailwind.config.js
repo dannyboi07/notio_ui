1572021cf95f55d25beaab4b49541fcadbc1055f
dannyboi07 <danielchettiar07@gmail.com>
2023-06-24 13:16:45 +0530

    Changes font, colors in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,28 @@
+const colors = require("tailwindcss/colors");
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {
             colors: {
+                black: {
+                    // Black
+                    900: colors.neutral[900],
+                    700: colors.neutral[700],
+                    // Ex. Login email field's font colour
+                    400: colors.stone[400],
+                    // Ex. Login email field's bg colour
+                    300: colors.stone[300],
+                },
                 blue: {
                     100: "#caf0f8",
                     200: "#ade8f4",
                     300: "#90e0ef",
-                    400: "#48cae4",
+                    // Ex. Login email field's focus border colour
+                    // 400: "#48cae4",
+                    400: colors.sky[400],
                     500: "#00b4d8",
+                    // 500: colors.sky[500],
                     600: "#0096c7",
                     700: "#0077b6",
                     800: "#023e8a",
@@ -27,7 +40,9 @@ export default {
             },
         },
         fontFamily: {
-            sans: ["Merriweather Sans", "sans"],
+            sans: ["Nunito Sans", "sans"],
+            // sans: ["Merriweather Sans", "sans"],
+            // sans: ["Raleway", "sans"],
         },
     },
     plugins: [],
