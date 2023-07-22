f1379fa83f8099ff359f2971172c9b7bde43cf5b
dannyboi07 <danielchettiar07@gmail.com>
2023-07-23 00:10:38 +0530

    Temorary: Disabling cookie refresh until kink is worked out

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,8 +1,31 @@
 import axios from "axios";
+// import type { AxiosError } from "axios";
 import config from "../../config";
 
 const axiosInstance = axios.create({
     baseURL: `${config.BASE_API_URL}${config.BASE_API_URI}`
 });
+
+// let refreshed = false;
+
+// axiosInstance.interceptors.response.use(
+//     (response) => {
+//         return response;
+//     },
+//     async (error: AxiosError) => {
+//         if (error.response?.status === 401 && !refreshed) {
+//             try {
+//                 refreshed = true;
+//                 await axiosInstance.get("/auth/refresh", {
+//                     withCredentials: true
+//                 });
+//                 return axiosInstance.request(error.config);
+//             } catch (err: AxiosError) {
+//                 return Promise.reject(err);
+//             }
+//         }
+//         return Promise.reject(error);
+//     }
+// );
 
 export default axiosInstance;
