42174f64e74bf895aca97589d7b7224d3256d274
dannyboi07 <danielchettiar07@gmail.com>
2023-07-27 00:28:19 +0530

    Adds `<BoardCard />` component

---
+++ src/components/Board/BoardCard.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+interface BoardCardProps {
+    kanbanBoard: KanbanBoard;
+    wrapperClassName?: string;
+    wrapInLink?: boolean;
+    linkToHref?: string;
+}
+
+function BoardCard({
+    kanbanBoard,
+    wrapperClassName = "",
+    wrapInLink = false,
+    linkToHref = "",
+}: BoardCardProps) {
+    const core = (
+        <div
+            className={`flex h-fit min-h-[100px] w-fit min-w-[300px] flex-col gap-y-1 rounded-md border border-black-300 px-3 py-1.5 transition-colors hover:bg-slate-100 ${wrapperClassName}`}
+        >
+            <h3 className="font-bold">{kanbanBoard.title}</h3>
+            <span className="h-[1px] w-[90%] self-center bg-black-300" />
+            <p className="text-sm">{kanbanBoard.description}</p>
+        </div>
+    );
+
+    if (wrapInLink) {
+        return (
+            <a href={linkToHref} className="h-fit w-fit">
+                {core}
+            </a>
+        );
+    }
+    return core;
+}
+
+export default BoardCard;
