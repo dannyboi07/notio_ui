c8d65c4d81c73ad47bf607f8fa6e1de120e9841c
dannyboi07 <danielchettiar07@gmail.com>
2023-07-27 00:26:37 +0530

    Updates navbar layout's spacing

--- src/layouts/NavbarLayout.tsx
+++ src/layouts/NavbarLayout.tsx
@@ -6,7 +6,9 @@ function NavbarLayout({ children }: { children: React.ReactNode }) {
     return (
         <AuthLayout>
             <Navbar />
-            <main className="h-[calc(100%-48px)]">{children}</main>
+            <main className="flex h-[calc(100%-48px)] flex-col px-6 py-2">
+                {children}
+            </main>
         </AuthLayout>
     );
 }
